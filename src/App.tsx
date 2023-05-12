c8f0050cb54f9f7d7dcd87234d83c3c0a23bcc25
rodnring <aryan.shamsinejad@gmail.com>
2023-05-12 13:06:22 +0330

    chore: initial set-up, configuring basic frontend tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import tailwindLogo from './assets/tailwind.svg'
 import './App.css'
 
 function App() {
@@ -8,19 +9,31 @@ function App() {
 
   return (
     <>
-      <div>
+      <div >
+        <div className="flex justify-center flex-col gap-y-3">
+          <h1> Archetype Project </h1>
+          <h2> Powered by </h2>
+        </div>
+        
+        <div className="flex justify-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={tailwindLogo} className="logo react" alt="React logo" />
+        </a>  
+        </div>
+        
       </div>
-      <h1>Vite + React</h1>
+      <h2 className="text-slate-300 font-bold">Vite + React + Tailwind</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        Button
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
